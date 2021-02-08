b0d39b7e1dc27a301fba98587ce73d75c00e27ee
Bohdan Busko <bogdan.busko@gmail.com>
2021-02-08 13:25:41 +0200

    Update params in sessions_controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::SessionsController < Api::V1::BaseController
   before_action :authenticate_user!, only: :destroy
 
   def create
-    user = User.find_by(email: params[:email])
+    user = User.find_by(email: params[:session][:email])
 
-    if user&.valid_password?(params[:password])
+    if user&.valid_password?(params[:session][:password])
       render json: SessionSerializer.new(user), status: 201
     else
       head(:unauthorized)
