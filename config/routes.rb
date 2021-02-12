7c78b373cbc48bc1e51b5bc735178e30b66fec0a
Bohdan Busko <bogdan.busko@gmail.com>
2021-02-12 17:55:26 +0200

    Replace resources with resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :account do
-        resources :profiles, only: :show
+        resource :profiles, only: :show
         resources :organizations, only: :create
       end
 
