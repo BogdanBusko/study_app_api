b81ebd015abd119446c9749d345002372ac1f682
Bohdan Busko <busko.bogdan@gmail.com>
2021-03-24 13:41:29 +0200

    Spec refactoring

--- spec/requests/api/v1/sessions_controller_spec.rb
+++ spec/requests/api/v1/sessions_controller_spec.rb
@@ -7,37 +7,40 @@ RSpec.describe Api::V1::SessionsController, type: :request do
     before do
       post '/api/v1/sessions', params: params
     end
+    let!(:params) do
+      {
+        session: {
+          email: user_email,
+          password: user_password
+        }
+      }
+    end
 
     subject { response }
 
-    context 'with invalid params' do
-      let!(:params) do
-        {
-          session: {
-            email: user.email,
-            password: '12356'
-          }
-        }
-      end
+    context 'with invalid password' do
+      let!(:user_email) { user.email }
+      let!(:user_password) { '12345' }
 
       it { is_expected.to have_http_status(401) }
     end
 
     context 'with valid params' do
-      let!(:params) do
-        {
-          session: {
-            email: user.email,
-            password: '123456'
-          }
-        }
-      end
+      let!(:user_email) { user.email }
+      let!(:user_password) { '123456' }
 
       it { is_expected.to have_http_status(201) }
 
-      it 'returns user token if credentials valid' do
+      it 'returns user token' do
         expect(JSON.parse(subject.body)['data']['attributes']['token']).to_not be_nil
       end
+    end
+
+    context 'with invalid email' do
+      let!(:user_email) { 'john.doe@email.com' }
+      let!(:user_password) { '123456' }
+
+      it { is_expected.to have_http_status(401) }
     end
   end
 end
