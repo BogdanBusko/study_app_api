935fe39fb3eae84afae4f81482ca354cc3bdcd4d
Bohdan Busko <busko.bogdan@gmail.com>
2021-03-24 13:41:47 +0200

    Fix bug messod missing when user not found

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SessionsController < Api::V1::BaseController
   def create
     user = User.find_by(email: params[:session][:email])
 
-    if user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       render json: SessionSerializer.new(user), status: 201
     else
       head(:unauthorized)
